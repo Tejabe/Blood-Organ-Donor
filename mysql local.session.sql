3ef6216e6c9d1954e41026ec4cb8a956cf7fb7de
Tejabe <145987418+Tejabe@users.noreply.github.com>
2025-04-30 20:55:20 +0530

    Add files via upload

---
+++ mysql local.session.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   username VARCHAR(255) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   email VARCHAR(255) NOT NULL
+);
+INSERT INTO users (username, password, email)
+VALUES ('blood', 'blood', 'blood@example.com'),
+   ('karthi', 'karthi', 'karthi@gmail.com');
+SELECT *
+FROM users;
